c251924ef42b2977624da5a238ee5085adc94d3b
AzadTom <kumarazad2917@gmail.com>
2025-01-18 23:40:24 +0530

    made initial changes to layout added navbar.

--- src/components/blogs/Blogs.tsx
+++ src/components/blogs/Blogs.tsx
@@ -3,7 +3,7 @@ import BlogItem from "@/components/blogs/BlogItem";
 
 const Blogs = (props: { posts: BlogResponse }) => {
   return (
-    <section className="mx-4 my-4 grid grid-cols-2 gap-4">
+    <section className="mx-4 my-4 grid sm:grid-cols-2 gap-4">
       {props.posts.list.map((post: List) => (
         <BlogItem key={post.title} data={post} />
       ))}
